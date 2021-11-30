587eef7031c0a20a131c51595834851e7be45819
star07078 <577911127@qq.com>
2021-11-30 13:44:27 +0800

    123

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 // var decision = require('./decision.png');
 
 var app = express();
+app.use(express.static('static'))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
