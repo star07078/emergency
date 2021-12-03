cafe1a9d30c82c5200ace60d631509d5c00f53c5
star07078 <577911127@qq.com>
2021-12-03 17:22:50 +0800

    213

--- routes/index.js
+++ routes/index.js
@@ -70,7 +70,7 @@ router.post('/getResult', async (req, res) => {
       setTimeout(()=>{
         ec.exec('termux-media-player play ' + path.resolve(__dirname, '../static/tixing.wav'))
         wsd.send(JSON.stringify({ status: 200, str, name: req.body.name, level: level, decisionPath }))
-      },1000)
+      },5000)
       res.json({ status: 200, level: level, decisionPath })
     })
   } else {
@@ -78,7 +78,7 @@ router.post('/getResult', async (req, res) => {
       ec.exec('termux-media-player play ' + path.resolve(__dirname, '../static/tixing.wav'))
       // play.sound(path.resolve(__dirname, '../static/tixing.wav'))
       wsd.send(JSON.stringify({ status: 200,str, name: req.body.name, level: level }))
-    },1000)
+    },5000)
     res.json({ status: 200, level: level })
   }
 })
